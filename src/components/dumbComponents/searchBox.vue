5cd5b7e03ccded7aa3ab4e2ba72488a8ee07465f
SampsonM <SampsonM@github.com>
2019-01-08 21:20:35 +0000

    Created simple nav bar

---
+++ src/components/dumbComponents/searchBox.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="search-box">
+    <input :type="type" :name="name">
+    <button @click="handleClick">
+      <i class="fas fa-search"/>
+    </button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'SearchBox',
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    handleClick(event) {
+      this.$emit('search', event)
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import '../../styles/colours';
+
+.search-box {
+  display: flex;
+  margin: 5px;
+
+  button {
+    color: white;
+    background-color: $lightgreen;
+    outline: none;
+    border-radius: 0 5px 5px 0;
+    border: 2px solid white;
+  }
+
+  input {
+    border-style: none;
+  }
+}
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+
+    <SearchBox 
+      :search="handleSearch"
+      type="text"
+      name="SearchBox"
+    />
+
+  </div>
+</template>
+
+<script>
+import SearchBox from './dumbComponents/searchBox';
+
+export default {
+  name: 'Nav',
+  components: {
+    SearchBox
+  },
+  methods: {
+    handleSearch: (value) => {
+      console.log(value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../styles/colours';
+
+#nav {
+  padding: 15px 30px;
+  background-color: $green;
+  display: flex;
+
+  a {
+    font-weight: bold;
+    color: $blue;
+    margin: 10px;
+
+    &:first-of-type {
+      border-right: 1px solid white;
+      padding-right: 10px;
+      margin-right: 0;
+    }
+
+    &.router-link-exact-active {
+      color: white;
+    }
+  }
+}
+</style>
